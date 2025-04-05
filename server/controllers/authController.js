9a1ac2ee4c9b263b7f620ccdff0f474b963c2548
vaibhav <codweird5@gmail.com>
2025-04-05 21:50:25 +0530

    added bcrypt

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,5 @@
 const User = require('../models/User');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 exports.signup = async (req, res) => {
   const { username, email, password, role } = req.body;
